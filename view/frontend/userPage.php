15cb136a69fd0617f600bbda1769eee3d3661c8d
Meiyo40 <loic.demesmay@gmail.com>
2019-05-22 10:20:36 +0200

    ADD(adminPanel): new user form

--- view/frontend/userPage.php
+++ view/frontend/userPage.php
@@ -5,7 +5,7 @@
             <script src='assets/js/comment.js'></script>
             <script src='assets/js/userPage.js'></script>";
     $headContent = "<link href='./assets/css/adminpanel.css' rel='stylesheet' type='text/css'>
-    <link href='./assets/css/usertable.css' rel='stylesheet' type='text/css'>
+    <link href='./assets/css/userPage.css' rel='stylesheet' type='text/css'>
     <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.8.2/css/all.css' integrity='sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay' crossorigin='anonymous'>";
 ?>
 <?php ob_start(); ?>
@@ -18,8 +18,59 @@
         <a href="index.php?action=editarticle"><button><i class="far fa-edit"></i> Modifier un article</button></a>        
         <a href="index.php?action=moderation"><button><i class="far fa-comment-dots"></i> Gestion des commentaires</button></a>
         <a href="index.php?action=users"><button><i class="fas fa-user-graduate"></i> Gestion des utilisateurs</button></a>
-        <a href="index.php?action=adduser"><button><i class="fas fa-user-plus"></i> Ajouter un utilisateur</button></a>
     </div>
+
+    <div class="modal fade" id="modalRegisterForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header text-center">
+                <h4 class="modal-title w-100 font-weight-bold">Nouvel Utilisateur</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body mx-3">
+                <div class="md-form mb-5">
+                    <i class="fas fa-user prefix grey-text"></i>
+                    <input type="text" id="orangeForm-name" class="form-control validate">
+                    <label data-error="wrong" data-success="right" for="orangeForm-name">Nom utilisateur</label>
+                </div>
+
+                <div class="md-form mb-5">
+                    <i class="fas fa-envelope prefix grey-text"></i>
+                    <input type="email" id="orangeForm-email" class="form-control validate">
+                    <label data-error="wrong" data-success="right" for="orangeForm-email">Email utilisateur</label>
+                </div>
+
+                <div class="md-form mb-4">
+                    <i class="fas fa-lock prefix grey-text"></i>
+                    <input type="password" id="orangeForm-pass" class="form-control validate">
+                    <label data-error="wrong" data-success="right" for="orangeForm-pass">MDP utilisateur</label>
+                </div>
+
+                <div class="md-form mb-4">
+                    <i class="fas fa-user-graduate"></i>
+                    <label data-error="wrong" data-success="right" for="orangeForm-rank">Rang utilisateur: </label>
+                    <select>
+                        <option value='1'>Modérateur</option>
+                        <option value='2'>Autheur</option>
+                        <option value='3'>Admin</option>
+                    </select>                    
+                </div>
+
+            </div>
+            <div class="modal-footer d-flex justify-content-center">
+                <button class="btn btn-deep-orange"><i class="fas fa-user-plus"></i> Ajouter l'utilisateur</button>
+            </div>
+            </div>
+        </div>
+    </div>
+
+<div class="text-center">
+  <button type="button" class="btn btn-info">
+    <a id='user-add-btn' href="" class="btn btn-default btn-rounded" data-toggle="modal" data-target="#modalRegisterForm"><i class="fas fa-user-plus"></i> Ajouter un utilisateur</a>
+  </button>
+</div>
 
     <table id='userstable'>
         <thead>
