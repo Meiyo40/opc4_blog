1c9460899c66cd7571f7e75b180566a1d68a887e
Meiyo40 <loic.demesmay@gmail.com>
2019-05-22 14:52:58 +0200

    Fix js error

--- assets/js/comment.js
+++ assets/js/comment.js
@@ -1,15 +1,19 @@
 let commentArea = document.getElementById('comment-form');
-commentArea.style.display = "none";
+if(commentArea){
+    commentArea.style.display = "none";
+}
 
 let btnPost = document.getElementById('btnPost');
-btnPost.addEventListener('click', function(){
-    commentArea.style.display = "block";
-    commentArea.classList.add('post');
-    let pageId = commentArea.getAttribute('value');
-    let depth = document.getElementById('node-depth-form');
-    depth.setAttribute('value', '0');
-    commentArea.action = "index.php?action=post&id=" + pageId + "&comment=primary";
-});
+if(btnPost){
+    btnPost.addEventListener('click', function(){
+        commentArea.style.display = "block";
+        commentArea.classList.add('post');
+        let pageId = commentArea.getAttribute('value');
+        let depth = document.getElementById('node-depth-form');
+        depth.setAttribute('value', '0');
+        commentArea.action = "index.php?action=post&id=" + pageId + "&comment=primary";
+    });
+}
 
 function displayForm(commentId, node_depth){
     commentArea.style.display = "block";
